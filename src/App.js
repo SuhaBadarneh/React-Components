664fffdb6c2992b7ce1147c0f506b1e90bac1f53
unknown <suha.a.badarneh@gmail.com>
2023-07-01 22:40:48 +0300

    Upload  React Components folder

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import FlashCards from "./components/FlashCards";
+import Counter from "./components/Counter";
+import CounterVersion2 from "./components/CounterVersion2";
+
+function App() {
+  return (
+    <div className="App">
+      <CounterVersion2 />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Counter.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <button onClick={() => setCount((c) => c - 1)}> - </button> Counter :{" "}
+      {count}
+      <button onClick={() => setCount((c) => c + 1)}> + </button>
+    </>
+  );
+}
